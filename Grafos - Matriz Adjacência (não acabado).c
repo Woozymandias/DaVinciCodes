c9d4b7ae0bc48ffc972c359520290cf4d61368ad
Woozymandias <61988518+Woozymandias@users.noreply.github.com>
2020-05-25 19:13:52 -0300

    Update Grafos - Matriz Adjacência (não acabado).c

--- Grafos - Matriz Adjacência (não acabado).c
+++ Grafos - Matriz Adjacência (não acabado).c
@@ -5,38 +5,38 @@ void carregar_grafo(void){
 
     FILE* grafo = fopen("entrada.txt","r");
     char linha[10];
-    int g[2],l[3];  //g irá guardar numero de vertices e arestas, respectivamente, enquanto L guardará os 3 dados de cada linha
-    int i,j,aux,compar; 
+    int g[2],l[3];
+    int i,j,aux,compar;
     fgets(linha,sizeof(linha),grafo);
     i = 0;
     j = 0;
     aux = 0;
-    printf("%s\n",linha);   //teste para verificar se o fgets funcionou
-    while(linha[i] != '\0'){    //laço para definição do número de vértices e arestas
+    printf("%s",linha);
+    while(linha[i] != '\0'){
         if(linha[i] == ' '){
             g[j] = aux;
             j++;
             aux = 0;
-        }else if(((int)linha[i] >= 48) && ((int)linha[i] <= 57)){
+        }else if((int)linha[i] >= 48 && (int)linha[i] <= 57){
             aux = (aux * 10) + ((int)linha[i] - 48);
         }
         i++;
     }
-    printf("%d %d\n",g[1],g[2]);    //teste para verificar se as operações no laço de repetição funcionaram
-    int** mat = (int**)malloc(g[1]*sizeof(int*));
-    for(i=0;i<g[1];i++)
-        mat[i] = (int*)malloc(g[1]*sizeof(int));
-    for(i=0;i<g[1];i++){
-        for(j=0;j<g[1];j++)
-            mat[i][j] = 0;  //a matriz é preenchida com 0
+    printf("%d %d\n",g[0],g[1]);
+    int** mat = (int**)malloc(g[0]*sizeof(int*));
+    for(i=0;i<g[0];i++)
+        mat[i] = (int*)malloc(g[0]*sizeof(int));
+    for(i=0;i<g[0];i++){
+        for(j=0;j<g[0];j++)
+            mat[i][j] = 0;
     }
-    while(fgets(linha,sizeof(linha),grafo) != NULL){    //laço para posicionamento dos pesos na matriz
+    while(fgets(linha,sizeof(linha),grafo) != NULL){
         i = 0;
         j = 0;
         aux = 0;
         compar = 0;
         while(linha[i] != '\0'){
-            if(linha[i] == ' ' && (int)linha != 45){
+            if(linha[i] == ' '){
                 if(compar == 1)
                     aux = aux*(-1);
                 l[j] = aux;
@@ -49,11 +49,11 @@ void carregar_grafo(void){
             }
             i++;
         }
-        mat[l[1]-1][l[2]-1] = l[3];
-        mat[l[2]-1][l[1]-1] = l[3];
+        mat[l[0]-1][l[1]-1] = l[2];
+        mat[l[1]-1][l[0]-1] = l[2];
     }
-    for(i=0;i<g[1];i++){
-        for(j=0;j<g[1];j++)
+    for(i=0;i<g[0];i++){
+        for(j=0;j<g[0];j++)
             printf("%d ",mat[i][j]);
         puts("");
     }
